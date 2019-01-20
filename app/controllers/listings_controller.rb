213d6b938dea70de712905197da69594d4f00588
Matt <mattblesoe2@gmail.com>
2019-01-20 16:16:17 -0500

    add navbar, in page navigation, edit show destroy update functionality

---
+++ app/controllers/listings_controller.rb
@@ -0,0 +1,51 @@
+class ListingsController < ApplicationController
+  before_action :find_listing, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @listings = Listing.all.order("created_at DESC")
+  end
+
+  def show
+  end
+
+  def new
+    @listing = Listing.new
+  end
+
+  def create
+    @listing = Listing.new(listing_params)
+    if @listing.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @listing.update(listing_params)
+      redirect_to listing_path(@listing)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @listing.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def listing_params
+    params.require(:listing).permit(:title)
+  end
+
+  def find_listing
+    @listing = Listing.find(params[:id])
+  end
+
+end
